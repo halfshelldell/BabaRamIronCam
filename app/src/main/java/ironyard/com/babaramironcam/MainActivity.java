30a24c5f4c16a02e995c51a7d7fb467d19b009e8
halfshelldell <simmonslydell@gmai.com>
2016-08-04 14:38:19 -0400

    broken, will fix

--- app/src/main/java/ironyard/com/babaramironcam/MainActivity.java
+++ app/src/main/java/ironyard/com/babaramironcam/MainActivity.java
@@ -1,13 +1,48 @@
 package ironyard.com.babaramironcam;
 
+
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    SurfaceView surfaceView;
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        camera = Camera.open();
+        SurfaceHolder holder = surfaceView.getHolder();
+        holder.addCallback(this);
+        try {
+            camera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
